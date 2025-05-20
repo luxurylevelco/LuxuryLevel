0cf7003d5e9731c74a5977af8d189cf219ad9172
Ikaru Berjuega <francisjamespberjuega@gmail.com>
2025-05-20 15:39:57 +0800

    fixed navbar render issue

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import About from "@/components/homepage/about-us";
 import Hero from "@/components/homepage/hero";
 import Maps from "@/components/homepage/maps";
 
-export default function Page() {
+export default async function Page() {
   return (
     <>
       <Hero />
