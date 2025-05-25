8dff476a8f228ad6a3e38e902ef9ca5ba0b1ab28
Ikaru Berjuega <francisjamespberjuega@gmail.com>
2025-05-25 22:23:10 +0800

    optimized seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,9 +11,53 @@ const poppins = Poppins({
 });
 
 export const metadata: Metadata = {
-  title: "Luxury Level",
+  title: "Luxury Level – Premium Watches, Jewelry, and Designer Bags",
   description:
-    "Luxury Level a shop for jewelry and luxury watches based on Kuwait City, Kuwait",
+    "Discover timeless luxury at Luxury Level. Shop authentic watches, fine jewelry, and designer bags from Kuwait’s premier retailer, located in Al Zumorrodah Tower.",
+  keywords: [
+    "Luxury watches Kuwait",
+    "Designer bags Kuwait",
+    "Jewelry store Kuwait",
+    "Rolex Kuwait",
+    "Luxury Level",
+    "Al Zumorrodah tower",
+    "Patek Philippe",
+    "Authentic luxury items",
+  ],
+  authors: [
+    { name: "Luxury Level", url: `${process.env.NEXT_PUBLIC_FRONTEND_URL!}` },
+  ],
+  creator: "Luxury Level",
+  publisher: "Luxury Level",
+  applicationName: "Luxury Level Online Store",
+  icons: {
+    icon: "/favicon.ico",
+  },
+  openGraph: {
+    title: "Luxury Level – Premium Watches, Jewelry, and Designer Bags",
+    description:
+      "Explore Kuwait’s trusted source for authentic luxury goods. Rolex, Patek Philippe, Cartier, Chanel, and more.",
+    url: `${process.env.NEXT_PUBLIC_FRONTEND_URL!}`,
+    siteName: "Luxury Level",
+    type: "website",
+    images: [
+      {
+        url: `${process.env.NEXT_PUBLIC_FRONTEND_URL!}/banners/watches`, // replace with actual
+        width: 1200,
+        height: 630,
+        alt: "Luxury Level – Elegant watches and accessories",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Luxury Level – Premium Watches, Jewelry, and Designer Bags",
+    description:
+      "Shop authentic luxury timepieces, fine jewelry, and designer handbags from Kuwait’s premier luxury store.",
+    images: [`${process.env.NEXT_PUBLIC_FRONTEND_URL!}/banners/watches`], // replace with actual
+    creator: "@LuxuryLevel", // optional: your Twitter handle
+  },
+  metadataBase: new URL(process.env.NEXT_PUBLIC_FRONTEND_URL!),
 };
 
 export default async function RootLayout({
@@ -27,15 +71,15 @@ export default async function RootLayout({
 
   try {
     const [brandsRes, jewelryRes, bagsRes] = await Promise.all([
-      fetch(`${process.env.NEXT_PUBLIC_API_URL}//brands`, { method: "GET" }),
+      fetch(`${process.env.NEXT_PUBLIC_API_URL}/brands`, { method: "GET" }),
       fetch(
-        `${process.env.NEXT_PUBLIC_API_URL}//categories/jewelry/sub-categories`,
+        `${process.env.NEXT_PUBLIC_API_URL}/categories/jewelry/sub-categories`,
         {
           method: "GET",
         }
       ),
       fetch(
-        `${process.env.NEXT_PUBLIC_API_URL}//categories/bags/available-brands`,
+        `${process.env.NEXT_PUBLIC_API_URL}/categories/bags/available-brands`,
         {
           method: "GET",
         }
