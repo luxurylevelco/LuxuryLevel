ee7504a3a9595205d9d704ebb08e21884b0d5e12
luxurylevelco <luxurylevelco00@gmail.com>
2025-05-27 10:19:43 +0800

    changed footer texts (email), hero pic and banner pictures

--- components/homepage/hero.tsx
+++ components/homepage/hero.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function Hero() {
   return (
-    <div className="section-style relative bg-[url('/homepage-assets/hero.webp')] bg-cover bg-center bg-no-repeat p-20 bg-black">
+    <div className="section-style relative bg-[url('/homepage-assets/hero.avif')] bg-no-repeat bg-[center_top_20px] bg-[length:800px_auto] md:bg-[length:auto] p-20 bg-[#000100] ">
       <div className="absolute bottom-14 left-10 md:bottom-28 md:left-24 flex flex-col h-fit">
         <Link href={getWhatsAppUrl({ message: "" })} target="_blank">
           <div className="w-[100px]">
